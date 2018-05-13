5fc3c302a36babe75dbe9ebae982fc00eff0cee9
Mark Clifton <mark@phasespacedesign.com>
2018-05-13 11:44:49 -0400

    Added starter template files and ECE submodule

---
+++ design-documents/README.md
@@ -0,0 +1,9 @@
+# Emory University - Design Documents
+
+Place any source files commonly used in this project so that others may use and contribute. For example:
+
+* Photoshop
+* Illustrator
+* InDesign
+* MS Paint :)
+* ... and other files that don't go directly into the website
---
+++ src/js/site.js
@@ -0,0 +1,68 @@
+(function ($) {
+    if (!$) throw "jQuery library is required."
+
+    //Global reference
+    const timeoutDelay = 50;
+    const $win = $(window);
+    const $doc = $(document);
+    const $body = $("body");
+
+    //Cached references
+    let $siteHeader = $("#site-header");
+    let $dropdownToggles = $("#site-header .dropdown-toggle");
+    let $tooltip = $('[data-toggle="tooltip"]');
+    let $popover = $('[data-toggle="popover"]')
+
+    //Timeout Vars
+    let scrollTimeout;
+    let resizeTimeout;
+
+    //scrollFn
+    const scrollFn = function () {
+        //Modify header class when scrolling down
+        if ( $doc.scrollTop() > $siteHeader.height() ) {
+            $siteHeader.addClass("scroll");
+        } else {
+            $siteHeader.removeClass("scroll");
+        }
+        return null;
+    };
+
+    //resizeFn
+    const resizeFn = function () {
+        //Push body away from fixed header
+        $body.css("padding-top", $siteHeader.height());
+    }
+
+    //Setup listeners
+    $doc.scroll(function () {
+        clearTimeout(scrollTimeout);
+        scrollTimeout = setTimeout(scrollFn, timeoutDelay);
+    });
+
+    $win.resize(function () {
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(resizeFn, timeoutDelay * 4);
+    });
+
+    //Ready function
+    $doc.ready(function () {
+        //Ensure cached references are complete
+        $siteHeader = $("#site-header");
+        $dropdownToggles = $("#site-header .dropdown-toggle");
+
+        //Trigger resize events when page loads
+        $win.resize();
+
+        // Init BS tooltips and popovers everywhere
+        $tooltip.tooltip();
+        $popover.popover();
+
+        // Init scroll spy on target
+        $body.scrollspy({
+            offset: 100,
+            target: '#scrollspy'
+        });
+    });
+
+})(jQuery);
